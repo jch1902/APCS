1cbebbbb21453939882a5546cc8d0b63405734c5
Jasper <jasper.d39c@gmail.com>
2019-09-05 13:03:52 -0700

    initial

--- sphero copy/DrawHouse.java
+++ sphero copy/DrawHouse.java
@@ -2,10 +2,24 @@
 /**
  * Write a description of class DrawHouse here.
  *
- * @author (your name)
+ * @author (Jasper Hoong)
  * @version (a version number or a date)
  */
+import apcslib.*;
 public class DrawHouse
 {
-    
+    public static void main (String[]args){
+        SketchPad paper = new SketchPad(1000,1000);
+        DrawingTool pen = new DrawingTool(paper);
+        
+        pen.up();
+        pen.move(-400,-250);
+        pen.down();
+        pen.move(400,-250);
+        pen.move(400,250);
+        pen.move(-400,250);
+        pen.move(-400,-250);
+        
+        pen.up();
+    }
 }
